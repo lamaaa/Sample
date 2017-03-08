27dac9ca4e12d23925920182f7c60e7e0c4573d6
unknown <>
2017-03-08 15:43:06 +0800

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -129,4 +129,20 @@ class UsersController extends Controller
         session()->flash('success', '恭喜您，激活成功！');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function followers($id)
+    {
+        $user = User::findOrFail($id);
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followings($id)
+    {
+        $user = User::findOrFail($id);
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
 }
